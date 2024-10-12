dec9c30330dcdad4990cf2d75b91d7d24a11c8cf
63565 <lnlhochuumeret@gmail.com>
2024-10-12 16:21:27 +0300

    6.10 2.5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("введи четырехзначное число");
-        int num = scanner.nextInt();
-        int a = num % 10;
-        int b = (num%100)/10;
-        int c = (num%1000)/100;
-        int d = num/1000;
-        int sum = a + b + c + d;
-        int umn = a * b * c * d;
-        System.out.println("сумма цифр: " + sum);
-        System.out.println("произведение цифр: " + umn);
+
+        Scanner scanner = new Scanner (System.in);
+        int k =scanner.nextInt();
+        int m =scanner.nextInt();
+        double a =k / 3.6;
+        System.out.println("километр в час =" +a);
+        if (a > m) {
+            System.out.println("Километры в час больше метров в секунду: ");
+        } else {
+            System.out.println("Метры в секунду больше километров в час: ");
+        }
     }
 }
