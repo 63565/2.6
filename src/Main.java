22de48248a81ab8fa440ffe22916b7a2cdf16e81
63565 <lnlhochuumeret@gmail.com>
2024-10-12 17:27:50 +0300

    6.10 4 13

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите числа");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double x = (b*b) - 4*a*c;
+        if (x<0) {
+            System.out.println("вещественных корней нет");
+        } else if (a==0){
+            System.out.println("Попробуй еще детка");
+        }
+        else {
+            System.out.println("вещественные корни есть");
 
-        System.out.println("Введите радиус круга ");
-        double radius = scanner.nextDouble();
-        double area1 = Math.PI * (radius * radius);
-        System.out.println("Площадь круга равна: " + area1);
-        System.out.println("Введите сторону квадрата");
-        double side = scanner.nextDouble();
-        double area2 = side *side ;
-        System.out.println("Площадь квадрата: "+area2);
-        if (area1 >area2 ) {
-            System.out.println("Площадь круга больше");
-        } else if (area1==area2) {
-            System.out.println("Площади равны");
-        } else {
-            System.out.println("Площадь квадрата больше");
         }
     }
 }
