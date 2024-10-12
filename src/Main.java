876548cac1fe6efe63b5368c76f5debd6cd2f821
63565 <lnlhochuumeret@gmail.com>
2024-10-12 15:07:21 +0300

    6.10 2.5

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 234;
+        System.out.println(a);
+        int b = 7;
+        System.out.println(b);
+        int div = a / b;
+        System.out.println(div);
+    }
+}
