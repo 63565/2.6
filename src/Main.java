01e79cef3273757ce0e917ef8a6be1068708dc38
63565 <lnlhochuumeret@gmail.com>
2024-10-12 16:05:36 +0300

    6.10 2.5

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("введи трехзначное число");
-
+        System.out.print("введи четырехзначное число");
         int num = scanner.nextInt();
-
         int a = num % 10;
-        int b = (num % 100) / 10;
-        int c = num / 100;
-
-
-        System.out.println("число в обратном порядке " + a + b + c);
+        int b = (num%100)/10;
+        int c = (num%1000)/100;
+        int d = num/1000;
+        int sum = a + b + c + d;
+        int umn = a * b * c * d;
+        System.out.println("сумма цифр: " + sum);
+        System.out.println("произведение цифр: " + umn);
     }
 }
